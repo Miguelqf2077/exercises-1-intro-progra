879ced48b6611eea3daf8a66f83063f165cfc43d
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-03-15 10:09:05 -0400

    prueba 13

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -24,7 +24,6 @@ void exercise_1(string s1, string s2, string s3) {
 void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
     A,B,C;
-    double A, C;
     cin >> A >> C;
 
     if (C== 0) {
