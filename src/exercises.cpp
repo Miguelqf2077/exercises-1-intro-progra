092edebe2f78e64c18ef9eb88ad847c745608b44
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-03-15 22:45:39 -0400

    prueba 27

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -13,7 +13,7 @@ void exercise_1(string s1, string s2, string s3) {
     s3;
   
      if (s1 < s2 && s1 < s3) {
-         cout <<s1;
+         cout<< s1;
     } else if (s2 < s1 && s2 < s3) {
          cout << s2;
     } else {
