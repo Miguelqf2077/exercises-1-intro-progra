67099a45f83c447f0f172c5a67d36af192d738cd
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-03-16 09:29:09 -0400

    PRUEBA 36

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -24,31 +24,30 @@ void exercise_1(string s1, string s2, string s3) {
 void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
    {
-    if (a == 0)
+    if (A == 0)
     {
-        double x = -c / b;
+        double x = -C / B;
         cout << "Unico valor para X: " << x << endl;
         return;
     }
     double divisor, raiz;
     int x1, x2;
-    // pow(BASE, EXPONENTE)
 
-    if (b * b - 4 * a * c < 0) // raiz(b * b - 4 * a * c )
+    if (B * B - 4 * A * C < 0) 
     {
-        divisor = 2 * a;
-        raiz = pow(-1 * (b * b - 4 * a * c), 0.5);
-        x1 = (-b + raiz) / divisor;
-        x2 = (-b - raiz) / divisor;
+        divisor = 2 * A;
+        raiz = pow(-1 * (B * B - 4 * A * C), 0.5);
+        x1 = (-B + raiz) / divisor;
+        x2 = (-B - raiz) / divisor;
         cout << "Valor para X1: " << x1 << " i" << endl;
         cout << "Valor para X2: " << x2 << " i" << endl;
     }
     else
     {
-        divisor = 2 * a;
-        raiz = pow(b * b - 4 * a * c, 0.5);
-        x1 = (-b + raiz) / divisor;
-        x2 = (-b - raiz) / divisor;
+        divisor = 2 * A;
+        raiz = pow(B * B - 4 * A * C, 0.5);
+        x1 = (-B + raiz) / divisor;
+        x2 = (-B - raiz) / divisor;
         cout << "Valor para X1: " << x1 << endl;
         cout << "Valor para X2: " << x2 << endl;
     }
