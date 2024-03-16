4984ebc7c6b36dc11f9b83f7b7331ba4d590763b
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-03-15 22:33:52 -0400

    PRUEBA 25

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -33,19 +33,18 @@ void exercise_2(double A, double B, double C) {
 
         double x1 = (-B + sqrt(x)) / (2 * A);
         double x2 = (-B - sqrt(x)) / (2 * A);
-        cout << "Las raíces son: " << x1 << " y " << x2 << endl;
+        
 
     } else if (x== 0) {
 
         double x = -B / (2 * A);
-        cout << "La raíz doble es: " << x << endl;
+        
 
     } else {
 
         double x1 = -B / (2 *A);
         double x2 = sqrt(-x) / (2 * A);
-        cout << "Las raíces son números complejos: " << x1 << " + " << x2 << "i y " 
-             << x1 << " - " << x2 << "i" << endl;
+       
     }
 
 }
