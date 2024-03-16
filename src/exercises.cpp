d6eebf664dd078bcfbe68fc9f27ffcb781ea9863
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-03-15 22:05:35 -0400

    prueba 21

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -23,6 +23,7 @@ void exercise_1(string s1, string s2, string s3) {
 
 void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
+  {
     A;
     B;
     C;
@@ -45,7 +46,7 @@ void exercise_2(double A, double B, double C) {
         cout<<"X2 = "<<X2<<endl; 
     }
 
-
+}
 
 
 }
