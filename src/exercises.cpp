50bc041d0636b3307bb4f0c6a2cc866b73bef992
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-03-16 15:10:46 -0400

    prueba 41

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -73,13 +73,43 @@ void exercise_4(double n, double a, double b, double x, double y) {
 
   
 }
-
 void exercise_5(char character) {
   // TODO: YOUR CODE HERE
+
+
+
+
 }
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
+  switch (number) {
+        case 1:
+            cout << "Lunes" << endl;
+            break;
+        case 2:
+            cout << "Martes" << endl;
+            break;
+        case 3:
+            cout << "Miercoles" << endl;
+            break;
+        case 4:
+            cout << "Jueves" << endl;
+            break;
+        case 5:
+            cout << "Viernes" << endl;
+            break;
+        case 6:
+            cout << "Sabado" << endl;
+            break;
+        case 7:
+            cout << "Domingo" << endl;
+            break;
+        default:
+            cout << "Número de día inválido" << endl;
+            break;
+    }
+
 }
 
 void exercise_7(double r) {
