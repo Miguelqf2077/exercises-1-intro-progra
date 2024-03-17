deca15dacd7b7b74e776ec93e36d25b90f154c48
Miguelqf2077 <161902082+Miguelqf2077@users.noreply.github.com>
2024-03-16 23:21:10 -0400

    PRUEBA 59

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -21,27 +21,27 @@ void exercise_1(string s1, string s2, string s3) {
     }
 }    
 
-void exercise_2(double a, double b, double c) {
+void exercise_2(double A, double B, double C) {
   // TODO: YOUR CODE HERE
-  if (a == 0 && b != 0)
+  if (A == 0 && B != 0)
     {
-        double x = -c / b;
+        double x = -C / B;
         cout << x << endl;
         return;
     }
     double divisor, raiz;
     double x1, x2;
 
-    if (b * b - 4 * a * c < 0 || (a == 0 && b == 0))
+    if (B * B - 4 * A * C < 0 || (A == 0 && B == 0))
     {
         cout << ""  ;
     }
     else
     {
         divisor = 2 * a;
-        raiz = pow(b * b - 4 * a * c, 0.5);
-        x1 = (-b + raiz) / divisor;
-        x2 = (-b - raiz) / divisor;
+        raiz = pow(B * B - 4 * A * C, 0.5);
+        x1 = (-B + raiz) / divisor;
+        x2 = (-B - raiz) / divisor;
         if (x1 == x2)
         {
             cout << x1 << endl;
